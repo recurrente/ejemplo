0c1693fc72c5bcd13f46de891f4315b97349d1b5
Alejo Rivera <alejo@alejorivera.com>
2021-04-30 20:39:58 +0200

    Break stuff

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,9 @@ export default function Home() {
             <a
               href="#"
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
-              data-recurrente-button
-              data-recurrente-price-id="1c8e3c3e"
+              data-recurrente-key="pk_test_naKXLC6Simkz7lF2ABmYKWAbj4QQ901GYH3n7g3hM1CJ01Uzy566WD2eh"
+              data-recurrente-quantity="2"
+              data-recurrente-price="1c8e3c3e"
             >
               Probar Checkout
           </a>
