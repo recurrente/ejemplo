8f2f25997ecd71f4330c6790ec53af7dbb08e448
Max Holzheu <maxhm10@gmail.com>
2021-05-05 09:01:07 -0600

    use unpckg

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <title>Recurrente Sample</title>
         <link rel="icon" href="/logo.png" />
-        <script src="../js/recurrente.js"></script>
+        <script type="module" src="https://unpkg.com/recurrente@latest"></script>
       </Head>
 
       <div className="">
