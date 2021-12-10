170da0c9ef21ace2848aa69d469482bea480cc98
Alejo <alejo@alejorivera.com>
2021-12-10 18:40:10 +0100

    New priceId

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,7 @@ export default function Home() {
               href="#"
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
               data-recurrente-button
-              data-recurrente-price-id="pr_4bfnflae"
-              data-recurrente-quantity="1"
+              data-recurrente-price-id="price_4bfnflae"
             >
               Probar Checkout
           </a>
